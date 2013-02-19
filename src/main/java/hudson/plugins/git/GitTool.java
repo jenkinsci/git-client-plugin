f1e1827ed9d34ff4bc88932985afd48521a22ae6
Mark Waite <mwaite@ptc.com>
2013-02-19 10:28:36 -0700

    Update Java source files with jalopy automated formatting

--- src/main/java/hudson/plugins/git/GitTool.java
+++ src/main/java/hudson/plugins/git/GitTool.java
@@ -1,34 +1,42 @@
 package hudson.plugins.git;
 
-import hudson.EnvVars;
-import hudson.Extension;
-import hudson.Functions;
-import hudson.XmlFile;
-import hudson.init.Initializer;
-import hudson.model.EnvironmentSpecific;
-import hudson.model.Items;
-import hudson.model.Node;
-import hudson.model.TaskListener;
-import hudson.slaves.NodeSpecific;
-import hudson.tools.ToolDescriptor;
-import hudson.tools.ToolInstallation;
-import hudson.tools.ToolProperty;
-import hudson.util.FormValidation;
-import jenkins.model.Jenkins;
-import net.sf.json.JSONObject;
-import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.QueryParameter;
-import org.kohsuke.stapler.StaplerRequest;
-
-import javax.servlet.ServletException;
 import java.io.File;
 import java.io.IOException;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.servlet.ServletException;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.StaplerRequest;
+
+import hudson.EnvVars;
+import hudson.Extension;
+import hudson.Functions;
+import hudson.XmlFile;
 import static hudson.init.InitMilestone.PLUGINS_STARTED;
+import hudson.init.Initializer;
+
+import hudson.model.EnvironmentSpecific;
+import hudson.model.Items;
+import hudson.model.Node;
+import hudson.model.TaskListener;
+
+import hudson.slaves.NodeSpecific;
+
+import hudson.tools.ToolDescriptor;
+import hudson.tools.ToolInstallation;
+import hudson.tools.ToolProperty;
+
+import hudson.util.FormValidation;
+
+import jenkins.model.Jenkins;
+
+import net.sf.json.JSONObject;
 
 /**
  * Information about Git installation.
@@ -36,13 +44,13 @@ import static hudson.init.InitMilestone.PLUGINS_STARTED;
  * @author Jyrki Puttonen
  */
 public final class GitTool extends ToolInstallation implements NodeSpecific<GitTool>, EnvironmentSpecific<GitTool> {
+    public static transient final String DEFAULT = "Default";
+    private static final Logger LOGGER = Logger.getLogger(GitTool.class.getName());
 
     @DataBoundConstructor
-    public GitTool(String name, String home, List<? extends ToolProperty<?>> properties) {
+    public GitTool(String name, String home, List<?extends ToolProperty<?>> properties) {
         super(name, home, properties);
     }
-
-    public static transient final String DEFAULT = "Default";
 
     public String getGitExe() {
         return getHome();
@@ -50,16 +58,18 @@ public final class GitTool extends ToolInstallation implements NodeSpecific<GitT
 
     private static GitTool[] getInstallations(DescriptorImpl descriptor) {
         GitTool[] installations = null;
+
         try {
             installations = descriptor.getInstallations();
         } catch (NullPointerException e) {
             installations = new GitTool[0];
         }
+
         return installations;
     }
 
-
-    public GitTool forNode(Node node, TaskListener log) throws IOException, InterruptedException {
+    public GitTool forNode(Node node, TaskListener log)
+        throws IOException, InterruptedException {
         return new GitTool(getName(), translateFor(node, log), Collections.<ToolProperty<?>>emptyList());
     }
 
@@ -74,13 +84,14 @@ public final class GitTool extends ToolInstallation implements NodeSpecific<GitT
 
     @Extension
     public static class DescriptorImpl extends ToolDescriptor<GitTool> {
-
         public DescriptorImpl() {
             super();
             setInstallations(new GitTool[0]);
             load();
+
             GitTool[] installations = getInstallations();
-            if (installations == null || installations.length == 0) {
+
+            if ((installations == null) || (installations.length == 0)) {
                 String defaultGitExe = Functions.isWindows() ? "git.exe" : "git";
                 GitTool tool = new GitTool(DEFAULT, defaultGitExe, Collections.<ToolProperty<?>>emptyList());
                 setInstallations(new GitTool[] { tool });
@@ -94,32 +105,31 @@ public final class GitTool extends ToolInstallation implements NodeSpecific<GitT
         }
 
         @Override
-        public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+        public boolean configure(StaplerRequest req, JSONObject json)
+            throws FormException {
             super.configure(req, json);
             save();
+
             return true;
         }
 
-        public FormValidation doCheckHome(@QueryParameter File value)
-            throws IOException, ServletException {
-
+        public FormValidation doCheckHome(@QueryParameter
+        File value) throws IOException, ServletException {
             Jenkins.getInstance().checkPermission(Jenkins.ADMINISTER);
+
             String path = value.getPath();
 
             return FormValidation.validateExecutable(path);
-
         }
 
         public GitTool getInstallation(String name) {
-            for(GitTool i : getInstallations()) {
-                if(i.getName().equals(name)) {
+            for (GitTool i : getInstallations()) {
+                if (i.getName().equals(name)) {
                     return i;
                 }
             }
+
             return null;
         }
     }
-
-    private static final Logger LOGGER = Logger.getLogger(GitTool.class.getName());
 }
-
