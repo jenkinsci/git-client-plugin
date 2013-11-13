b6d5c047f7ccf27a4913a241b70d181fae9ecbc7
Victor Barros <vbarros@atimi.com>
2013-11-13 12:57:45 -0800

    fix to support nested branches and similar branches with the same suffix. for example: develop beta/develop

--- src/main/java/org/jenkinsci/plugins/gitclient/CliGitAPIImpl.java
+++ src/main/java/org/jenkinsci/plugins/gitclient/CliGitAPIImpl.java
@@ -64,6 +64,8 @@ import static org.apache.commons.httpclient.params.HttpMethodParams.USER_AGENT;
  * </b>
  */
 public class CliGitAPIImpl extends LegacyCompatibleGitAPIImpl {
+    
+    public static final String REFS_HEADS = "refs/heads/";
 
     static {
         if (Boolean.getBoolean(GitClient.class.getName() + ".untrustedSSL")) {
@@ -1339,8 +1341,9 @@ public class CliGitAPIImpl extends LegacyCompatibleGitAPIImpl {
     }
 
     public ObjectId getHeadRev(String url, String branch) throws GitException, InterruptedException {
-        String[] branchExploded = branch.split("/");
-        branch = branchExploded[branchExploded.length-1];
+        if (!branch.startsWith(REFS_HEADS)) {
+            branch = String.format("%s%s", REFS_HEADS, branch.startsWith("/") ? branch.substring(1) : branch);
+        }
         ArgumentListBuilder args = new ArgumentListBuilder("ls-remote");
         args.add("-h");
 
