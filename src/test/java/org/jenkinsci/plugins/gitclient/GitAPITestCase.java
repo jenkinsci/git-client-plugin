9be657ec512d360337dd2ff72f450739762360bc
Victor Barros <vbarros@atimi.com>
2013-11-15 16:56:21 -0800

    handling if the name of the branch is starting with origin/

--- src/test/java/org/jenkinsci/plugins/gitclient/GitAPITestCase.java
+++ src/test/java/org/jenkinsci/plugins/gitclient/GitAPITestCase.java
@@ -890,6 +890,9 @@ public abstract class GitAPITestCase extends TestCase {
         final String headBetaDevelop = w.head().getName();
         
         assertEquals(headDevelop, w.git.getHeadRev(w.repoPath(), "develop").getName());
+        assertEquals(headDevelop, w.git.getHeadRev(w.repoPath(), "origin/develop").getName());
+        
         assertEquals(headBetaDevelop, w.git.getHeadRev(w.repoPath(), "beta/develop").getName());
+        assertEquals(headBetaDevelop, w.git.getHeadRev(w.repoPath(), "origin/beta/develop").getName());
     }
 }
