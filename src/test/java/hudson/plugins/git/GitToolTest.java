5952a6d22d90ec0a4db46719d8f3d6dbcfee97a7
Mark Waite <mark.earl.waite@gmail.com>
2014-05-08 07:36:20 -0600

    Add GitTool test

---
+++ src/test/java/hudson/plugins/git/GitToolTest.java
@@ -0,0 +1,55 @@
+package hudson.plugins.git;
+
+import hudson.EnvVars;
+import hudson.model.Node;
+import hudson.model.TaskListener;
+import hudson.slaves.DumbSlave;
+import hudson.util.StreamTaskListener;
+import java.io.IOException;
+import org.apache.commons.lang.SystemUtils;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+public class GitToolTest {
+
+    @Rule public JenkinsRule j = new JenkinsRule();
+
+    private GitTool gitTool;
+    
+    @Before
+    public void setUp() throws IOException {
+        GitTool.onLoaded();
+        gitTool = GitTool.getDefaultInstallation();
+    }
+    
+    @Test
+    public void testGetGitExe() {
+        assertEquals(SystemUtils.IS_OS_WINDOWS ? "git.exe" : "git", gitTool.getGitExe());
+    }
+
+    /* @Test - Need to find a way to create a Node in a unit test */
+    public void testForNode() throws Exception {
+        DumbSlave s = j.createSlave();
+        Node node = null;
+        TaskListener log = StreamTaskListener.fromStdout();
+        GitTool newTool = gitTool.forNode(node, log);
+        assertEquals(gitTool.getGitExe(), newTool.getGitExe());
+    }
+
+    @Test
+    public void testForEnvironment() {
+        EnvVars environment = new EnvVars();
+        GitTool newTool = gitTool.forEnvironment(environment);
+        assertEquals(gitTool.getGitExe(), newTool.getGitExe());
+    }
+
+    @Test
+    public void testGetDescriptor() {
+        GitTool.DescriptorImpl descriptor = gitTool.getDescriptor();
+        assertEquals("Git", descriptor.getDisplayName());
+    }
+    
+}
