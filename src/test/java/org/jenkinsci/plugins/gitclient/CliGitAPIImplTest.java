565849d62e6a310b1e9145fe8cc35d7b5d371684
Mark Waite <mark.earl.waite@gmail.com>
2014-06-15 08:06:45 -0600

    Add git version parsing test for git 2.0.0 as seen in Debian testing

--- src/test/java/org/jenkinsci/plugins/gitclient/CliGitAPIImplTest.java
+++ src/test/java/org/jenkinsci/plugins/gitclient/CliGitAPIImplTest.java
@@ -8,6 +8,7 @@ import java.lang.reflect.Method;
  * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
  */
 public class CliGitAPIImplTest extends GitAPITestCase {
+
     @Override
     protected GitClient setupGitAPI(File ws) throws Exception {
         return Git.with(listener, env).in(ws).using("git").getClient();
@@ -21,9 +22,9 @@ public class CliGitAPIImplTest extends GitAPITestCase {
     protected void runTest() throws Throwable {
         Method m = getClass().getMethod(getName());
 
-        if (m.getAnnotation(NotImplementedInCliGit.class)!=null)
+        if (m.getAnnotation(NotImplementedInCliGit.class) != null) {
             return; // skip this test case
-
+        }
         try {
             m.invoke(this);
         } catch (InvocationTargetException e) {
@@ -36,112 +37,149 @@ public class CliGitAPIImplTest extends GitAPITestCase {
     }
 
     class VersionTest {
+
         public boolean assertTrueOrFalse;
-        public int     major;
-        public int     minor;
-        public int     rev;
-        public int     bugfix;
+        public int major;
+        public int minor;
+        public int rev;
+        public int bugfix;
 
         public VersionTest(boolean assertTrueOrFalse, int major, int minor, int rev, int bugfix) {
             this.assertTrueOrFalse = assertTrueOrFalse;
-            this.major  = major;
-            this.minor  = minor;
-            this.rev    = rev;
+            this.major = major;
+            this.minor = minor;
+            this.rev = rev;
             this.bugfix = bugfix;
         }
     }
+
     private void doTest(String versionOutput, VersionTest[] versions) {
         CliGitAPIImpl git = new CliGitAPIImpl("git", new File("."), listener, env);
         git.computeGitVersion(versionOutput);
         for (int i = 0; i < versions.length; ++i) {
             if (versions[i].assertTrueOrFalse) {
-                assertTrue("Failed " + versionOutput, git.isAtLeastVersion(versions[i].major,
-                                                                           versions[i].minor,
-                                                                           versions[i].rev,
-                                                                           versions[i].bugfix));
-            }
-            else {
-                assertFalse("Passed " + versionOutput, git.isAtLeastVersion(versions[i].major,
-                                                                            versions[i].minor,
-                                                                            versions[i].rev,
-                                                                            versions[i].bugfix));
+                assertTrue("Failed " + versionOutput, git.isAtLeastVersion(
+                        versions[i].major,
+                        versions[i].minor,
+                        versions[i].rev,
+                        versions[i].bugfix));
+            } else {
+                assertFalse("Passed " + versionOutput, git.isAtLeastVersion(
+                        versions[i].major,
+                        versions[i].minor,
+                        versions[i].rev,
+                        versions[i].bugfix));
             }
         }
     }
+
     public void test_git_version_debian_wheezy() {
-        VersionTest[] versions = { new VersionTest(true,  1, 7, 10,  4),
-                                   new VersionTest(true,  1, 7, 10,  3),
-                                   new VersionTest(false, 1, 7, 10,  5) };
+        VersionTest[] versions = {
+            new VersionTest(true,  1, 7, 10, 4),
+            new VersionTest(true,  1, 7, 10, 3),
+            new VersionTest(false, 1, 7, 10, 5)
+        };
         doTest("git version 1.7.10.4", versions);
     }
 
     public void test_git_version_debian_testing() {
-        VersionTest[] versions = { new VersionTest(true,  2, 0,  0,  0),
-                                   new VersionTest(true,  1, 9, 99, 99),
-                                   new VersionTest(false, 2, 0,  1,  0) };
+        VersionTest[] versions = {
+            new VersionTest(true,  2, 0,  0,  0),
+            new VersionTest(true,  1, 9, 99, 99),
+            new VersionTest(false, 2, 0,  1,  0)
+        };
         doTest("git version 2.0.0.rc0", versions);
+        doTest("git version 2.0.0.rc2", versions);
+        doTest("git version 2.0.0", versions);
     }
 
     public void test_git_version_debian_testing_old() {
-        VersionTest[] versions = { new VersionTest(true,  1, 9,  0,  0),
-                                   new VersionTest(true,  1, 8, 99, 99),
-                                   new VersionTest(false, 1, 9,  1,  0) };
+        VersionTest[] versions = {
+            new VersionTest(true,  1, 9,  0,  0),
+            new VersionTest(true,  1, 8, 99, 99),
+            new VersionTest(false, 1, 9,  1,  0)
+        };
         doTest("git version 1.9.0", versions);
     }
 
     public void test_git_version_windows_1800() {
-        VersionTest[] versions = { new VersionTest(true,  1, 8,  0,  0),
-                                   new VersionTest(true,  1, 7, 99,  0),
-                                   new VersionTest(false, 1, 8,  1,  0) };
+        VersionTest[] versions = {
+            new VersionTest(true,  1, 8,  0, 0),
+            new VersionTest(true,  1, 7, 99, 0),
+            new VersionTest(false, 1, 8,  1, 0)
+        };
         doTest("git version 1.8.0.msysgit.0", versions);
     }
 
     public void test_git_version_windows_1840() {
-        VersionTest[] versions = { new VersionTest(true,  1, 8,  4,  0),
-                                   new VersionTest(true,  1, 8,  3, 99),
-                                   new VersionTest(false, 1, 8,  4,  1) };
+        VersionTest[] versions = {
+            new VersionTest(true,  1, 8, 4,  0),
+            new VersionTest(true,  1, 8, 3, 99),
+            new VersionTest(false, 1, 8, 4,  1)
+        };
         doTest("git version 1.8.4.msysgit.0", versions);
     }
 
     public void test_git_version_windows_1852() {
-        VersionTest[] versions = { new VersionTest(true,  1, 8,  5,  2),
-                                   new VersionTest(true,  1, 8,  5,  1),
-                                   new VersionTest(false, 1, 8,  5,  3) };
+        VersionTest[] versions = {
+            new VersionTest(true,  1, 8, 5, 2),
+            new VersionTest(true,  1, 8, 5, 1),
+            new VersionTest(false, 1, 8, 5, 3)
+        };
         doTest("git version 1.8.5.2.msysgit.0", versions);
     }
 
     public void test_git_version_windows_1900() {
-        VersionTest[] versions = { new VersionTest(true,  1, 9,  0,  0),
-                                   new VersionTest(true,  1, 8, 99,  0),
-                                   new VersionTest(false, 1, 9,  0,  1) };
+        VersionTest[] versions = {
+            new VersionTest(true,  1, 9,  0, 0),
+            new VersionTest(true,  1, 8, 99, 0),
+            new VersionTest(false, 1, 9,  0, 1)
+        };
         doTest("git version 1.9.0.msysgit.0", versions);
     }
 
+    public void test_git_version_windows_1920() {
+        VersionTest[] versions = {
+            new VersionTest(true,  1, 9, 2,  0),
+            new VersionTest(true,  1, 9, 1, 99),
+            new VersionTest(false, 1, 9, 2,  1)
+        };
+        doTest("git version 1.9.2.msysgit.0", versions);
+    }
+
     public void test_git_version_redhat_5() {
-        VersionTest[] versions = { new VersionTest(true,  1, 8,  2,  1),
-                                   new VersionTest(true,  1, 8,  2,  0),
-                                   new VersionTest(false, 1, 8,  2,  2) };
+        VersionTest[] versions = {
+            new VersionTest(true,  1, 8, 2, 1),
+            new VersionTest(true,  1, 8, 2, 0),
+            new VersionTest(false, 1, 8, 2, 2)
+        };
         doTest("git version 1.8.2.1", versions);
     }
 
     public void test_git_version_redhat_65() {
-        VersionTest[] versions = { new VersionTest(true,  1, 7,  1,  0),
-                                   new VersionTest(true,  1, 7,  0, 99),
-                                   new VersionTest(false, 1, 7,  1,  1) };
+        VersionTest[] versions = {
+            new VersionTest(true,  1, 7, 1,  0),
+            new VersionTest(true,  1, 7, 0, 99),
+            new VersionTest(false, 1, 7, 1,  1)
+        };
         doTest("git version 1.7.1", versions);
     }
 
     public void test_git_version_opensuse_13() {
-        VersionTest[] versions = { new VersionTest(true,  1, 8,  4,  5),
-                                   new VersionTest(true,  1, 8,  4,  4),
-                                   new VersionTest(false, 1, 8,  4,  6) };
+        VersionTest[] versions = {
+            new VersionTest(true,  1, 8, 4, 5),
+            new VersionTest(true,  1, 8, 4, 4),
+            new VersionTest(false, 1, 8, 4, 6)
+        };
         doTest("git version 1.8.4.5", versions);
     }
 
     public void test_git_version_ubuntu_13() {
-        VersionTest[] versions = { new VersionTest(true,  1, 8,  3,  2),
-                                   new VersionTest(true,  1, 8,  3,  1),
-                                   new VersionTest(false, 1, 8,  3,  3) };
+        VersionTest[] versions = {
+            new VersionTest(true,  1, 8, 3, 2),
+            new VersionTest(true,  1, 8, 3, 1),
+            new VersionTest(false, 1, 8, 3, 3)
+        };
         doTest("git version 1.8.3.2", versions);
     }
 }
