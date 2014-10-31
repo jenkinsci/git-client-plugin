5beda7fe81287437bfccf4657a49232cc8d49cfe
Mark Waite <mark.earl.waite@gmail.com>
2014-10-31 08:12:12 -0600

    Enable test_branchContainingRemote - broken by changes, needs work
    Also removed the ugly adaptCliGitClone method, replacing it with the
    more direct, and more clear "git.checkout()" call.

--- src/test/java/org/jenkinsci/plugins/gitclient/GitAPITestCase.java
+++ src/test/java/org/jenkinsci/plugins/gitclient/GitAPITestCase.java
@@ -247,27 +247,6 @@ public abstract class GitAPITestCase extends TestCase {
         public IGitAPI igit() {
             return (IGitAPI)git;
         }
-
-        /* CliGitAPIImpl.clone_() method does not set the remote URL,
-         * nor does it perform a checkout.  That is different than the
-         * default behavior of command line git, and different than
-         * the default behavior of the JGitAPIImpl.clone_() method.
-         * This convenience method adapts the CliGitAPIImpl clone
-         * results to be more consistent with the JGitAPIImpl clone
-         * results.
-         *
-         * TODO
-         * Since now Jgit does checkout using "init" and fetch as well
-         * Jgit behaviour actually is similar to CLI, not the other way
-         * around, i.e. right now, both implementations do not check out
-         * at the end, making this method obsolet (but test cases still
-         * rely on that behaviour).
-         */
-        void adaptCliGitClone(String repoName) throws IOException, InterruptedException {
-//            if (git instanceof CliGitAPIImpl) {
-                git.checkout(repoName + "/master", "master");
-//            }
-        }
     }
 
     private WorkingArea w;
@@ -458,18 +437,17 @@ public abstract class GitAPITestCase extends TestCase {
      *   repositoryName(String) - if omitted, CliGit does not set a remote repo name
      *   shallow() - no relevant assertion of success or failure of this argument
      *   shared() - not implemented on CliGit, not verified on JGit
-     *   reference() - not implemented on JGit, not verified on CliGit
+     *   reference() - implemented on JGit, not verified on either JGit or CliGit
      *
-     * CliGit requires the w.git.checkout() call otherwise no branch
-     * is checked out.  JGit checks out the master branch by default.
-     * That means JGit is nearer to command line git (in that case)
-     * than CliGit is.
+     * CliGit and JGit both require the w.git.checkout() call
+     * otherwise no branch is checked out. That is different than the
+     * command line git program, but consistent within the git API.
      */
     public void test_clone() throws IOException, InterruptedException
     {
         int newTimeout = 7;
         w.git.clone_().timeout(newTimeout).url(localMirror()).repositoryName("origin").execute();
-        w.adaptCliGitClone("origin");
+        w.git.checkout("origin/master", "master");
         check_remote_url("origin");
         assertBranchesExist(w.git.getBranches(), "master");
         final String alternates = ".git" + File.separator + "objects" + File.separator + "info" + File.separator + "alternates";
@@ -481,7 +459,7 @@ public abstract class GitAPITestCase extends TestCase {
     public void test_clone_repositoryName() throws IOException, InterruptedException
     {
         w.git.clone_().url(localMirror()).repositoryName("upstream").execute();
-        w.adaptCliGitClone("upstream");
+        w.git.checkout("upstream/master", "master");
         check_remote_url("upstream");
         assertBranchesExist(w.git.getBranches(), "master");
         final String alternates = ".git" + File.separator + "objects" + File.separator + "info" + File.separator + "alternates";
@@ -491,28 +469,27 @@ public abstract class GitAPITestCase extends TestCase {
     public void test_clone_shallow() throws IOException, InterruptedException
     {
         w.git.clone_().url(localMirror()).repositoryName("origin").shallow().execute();
-        w.adaptCliGitClone("origin");
+        w.git.checkout("origin/master", "master");
         check_remote_url("origin");
         assertBranchesExist(w.git.getBranches(), "master");
         final String alternates = ".git" + File.separator + "objects" + File.separator + "info" + File.separator + "alternates";
         assertFalse("Alternates file found: " + alternates, w.exists(alternates));
     }
 
-    /** shared is not implemented in CliGitAPIImpl. */
-    @NotImplementedInCliGit
-    @NotImplementedInJGit
     public void test_clone_shared() throws IOException, InterruptedException
     {
-        w.git.clone_().url(localMirror()).repositoryName("origin").shared().execute();
-        w.adaptCliGitClone("upstream");
-        check_remote_url("origin");
-        assertBranchesExist(w.git.getBranches(), "master");
+        try {
+            w.git.clone_().url(localMirror()).repositoryName("origin").shared().execute();
+            fail("Should have thrown an exception");
+        } catch (UnsupportedOperationException ue) {
+            assertEquals("shared is unsupported, and considered dangerous", ue.getMessage());
+        }
     }
 
     public void test_clone_reference() throws IOException, InterruptedException
     {
         w.git.clone_().url(localMirror()).repositoryName("origin").reference(localMirror()).execute();
-        w.adaptCliGitClone("origin");
+        w.git.checkout("origin/master", "master");
         check_remote_url("origin");
         assertBranchesExist(w.git.getBranches(), "master");
         final String alternates = ".git" + File.separator + "objects" + File.separator + "info" + File.separator + "alternates";
@@ -529,7 +506,7 @@ public abstract class GitAPITestCase extends TestCase {
     {
         assertTrue("SRC_DIR " + SRC_DIR + " has no .git subdir", (new File(SRC_DIR + File.separator + ".git").isDirectory()));
         w.git.clone_().url(localMirror()).repositoryName("origin").reference(SRC_DIR).execute();
-        w.adaptCliGitClone("origin");
+        w.git.checkout("origin/master", "master");
         check_remote_url("origin");
         assertBranchesExist(w.git.getBranches(), "master");
         final String alternates = ".git" + File.separator + "objects" + File.separator + "info" + File.separator + "alternates";
@@ -1595,7 +1572,7 @@ public abstract class GitAPITestCase extends TestCase {
      */
     private void base_checkout_replaces_tracked_changes(boolean defineBranch) throws Exception {
         w.git.clone_().url(localMirror()).repositoryName("JENKINS-23424").execute();
-        w.adaptCliGitClone("JENKINS-23424");
+        w.git.checkout("JENKINS-23424/master", "master");
         if (defineBranch) {
             w.git.checkout().branch("master").ref("JENKINS-23424/master").deleteBranchIfExist(true).execute();
         } else {
@@ -1656,7 +1633,7 @@ public abstract class GitAPITestCase extends TestCase {
     @Bug(8122)
     public void test_submodule_tags_not_fetched_into_parent() throws Exception {
         w.git.clone_().url(localMirror()).repositoryName("origin").execute();
-        w.adaptCliGitClone("origin");
+        w.git.checkout("origin/master", "master");
 
         String tagsBefore = w.cmd("git tag");
         Set<String> tagNamesBefore = w.git.getTagNames(null);
@@ -2666,7 +2643,6 @@ public abstract class GitAPITestCase extends TestCase {
      * testing case is retrieving remote branches.
      * @throws Exception on exceptions occur
      */
-    @NotImplementedInJGit
     public void test_branchContainingRemote() throws Exception {
         final WorkingArea r = new WorkingArea();
         r.init();
