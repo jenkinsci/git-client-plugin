fbb132d3ce3920244f9fe2d8a060ec218f04d8fc
Mark Waite <mark.earl.waite@gmail.com>
2014-11-29 06:46:13 -0700

    Fix Windows longpath tests on one problematic Win7 computer
    The changes are only cosmetic, and the root reason for the test failures
    is still unclear, since the same tests pass on other Windows 7 machines
    and on onther Windows machines.

--- src/test/java/org/jenkinsci/plugins/gitclient/GitAPITestCase.java
+++ src/test/java/org/jenkinsci/plugins/gitclient/GitAPITestCase.java
@@ -3213,44 +3213,82 @@ public abstract class GitAPITestCase extends TestCase {
      * JGit does not have that limitation.
      */
     public void check_longpaths(boolean longpathsEnabled) throws Exception {
-        String shortName = "0123456789abcdef";
+        String shortName = "0123456789abcdef" + "ghijklmnopqrstuv";
         String longName = shortName + shortName + shortName + shortName;
 
         String dirName1 = longName;
-        commitFile(dirName1, "file1", longpathsEnabled);
+        commitFile(dirName1, "file1a", longpathsEnabled);
 
         String dirName2 = dirName1 + File.separator + longName;
-        commitFile(dirName2, "file2", longpathsEnabled);
+        commitFile(dirName2, "file2b", longpathsEnabled);
 
         String dirName3 = dirName2 + File.separator + longName;
-        commitFile(dirName3, "file3", longpathsEnabled);
+        commitFile(dirName3, "file3c", longpathsEnabled);
 
         String dirName4 = dirName3 + File.separator + longName;
-        commitFile(dirName4, "file4", longpathsEnabled);
+        commitFile(dirName4, "file4d", longpathsEnabled);
 
         String dirName5 = dirName4 + File.separator + longName;
-        commitFile(dirName5, "file5", longpathsEnabled);
+        commitFile(dirName5, "file5e", longpathsEnabled);
+    }
+
+    private String getConfigValue(File workingDir, String name) throws IOException, InterruptedException {
+        String[] args = {"git", "config", "--get", name};
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        int st = new Launcher.LocalLauncher(listener).launch().pwd(workingDir).cmds(args).stdout(out).join();
+        String result = out.toString();
+        if (st != 0 && result != null && !result.isEmpty()) {
+            fail("git config --get " + name + " failed with result: " + result);
+        }
+        return out.toString().trim();
+    }
+
+    private String getHomeConfigValue(String name) throws IOException, InterruptedException {
+        return getConfigValue(new File(System.getProperty("user.home")), name);
+    }
+
+    private void assert_longpaths(boolean expectedLongPathSetting) throws IOException, InterruptedException {
+        String value = getHomeConfigValue("core.longpaths");
+        boolean longPathSetting = Boolean.valueOf(value);
+        assertEquals("Wrong value: '" + value + "'", expectedLongPathSetting, longPathSetting);
+    }
+
+    private void assert_longpaths(WorkingArea workingArea, boolean expectedLongPathSetting) throws IOException, InterruptedException {
+        String value = getConfigValue(workingArea.repo, "core.longpaths");
+        boolean longPathSetting = Boolean.valueOf(value);
+        assertEquals("Wrong value: '" + value + "'", expectedLongPathSetting, longPathSetting);
     }
 
     public void test_longpaths_default() throws Exception {
+        assert_longpaths(false);
         w.init();
+        assert_longpaths(w, false);
         check_longpaths(false);
+        assert_longpaths(w, false);
     }
 
     @NotImplementedInJGit
     /* Not implemented in JGit because it is not needed there */
     public void test_longpaths_enabled() throws Exception {
+        assert_longpaths(false);
         w.init();
-        w.cmd("git config --add core.longpaths true");
+        assert_longpaths(w, false);
+        w.cmd("git config core.longpaths true");
+        assert_longpaths(w, true);
         check_longpaths(true);
+        assert_longpaths(w, true);
     }
 
     @NotImplementedInJGit
     /* Not implemented in JGit because it is not needed there */
     public void test_longpaths_disabled() throws Exception {
+        assert_longpaths(false);
         w.init();
-        w.cmd("git config --add core.longpaths false");
+        assert_longpaths(w, false);
+        w.cmd("git config core.longpaths false");
+        assert_longpaths(w, false);
         check_longpaths(false);
+        assert_longpaths(w, false);
     }
 
     @NotImplementedInJGit
