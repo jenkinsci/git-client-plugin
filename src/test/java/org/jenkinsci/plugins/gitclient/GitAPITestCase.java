f6900736f255f7cf99c772b1002fb42b87371905
Mark Waite <mark.earl.waite@gmail.com>
2015-04-24 15:08:49 -0600

    Add test cyrillic file names from JENKINS-27910
    This does not address the core java library refusal to delete files
    named with unmappable characters. That needs a different solution, and
    a solution which would be better done in Jenkins core than in the git
    client plugin.

--- src/test/java/org/jenkinsci/plugins/gitclient/GitAPITestCase.java
+++ src/test/java/org/jenkinsci/plugins/gitclient/GitAPITestCase.java
@@ -722,6 +722,19 @@ public abstract class GitAPITestCase extends TestCase {
         w.git.add(fileName);
         w.git.commit(fileName);
 
+        /* JENKINS-27910 reported that certain cyrillic file names
+         * failed to delete if the encoding was not UTF-8.
+         */
+        String fileNameSwim = "\u00d0\u00bf\u00d0\u00bb\u00d0\u00b0\u00d0\u00b2\u00d0\u00b0\u00d0\u00bd\u00d0\u00b8\u00d0\u00b5-swim.png";
+        w.touch(fileNameSwim, "content " + fileNameSwim);
+        w.git.add(fileNameSwim);
+        w.git.commit(fileNameSwim);
+
+        String fileNameFace = "\u00d0\u00bb\u00d0\u00b8\u00d1\u2020\u00d0\u00be-face.png";
+        w.touch(fileNameFace, "content " + fileNameFace);
+        w.git.add(fileNameFace);
+        w.git.commit(fileNameFace);
+
         w.touch(".gitignore", ".test");
         w.git.add(".gitignore");
         w.git.commit("ignore");
@@ -739,6 +752,8 @@ public abstract class GitAPITestCase extends TestCase {
         assertFalse(w.exists(fileName1));
         assertFalse(w.exists(fileName2));
         assertEquals("content " + fileName, w.contentOf(fileName));
+        assertEquals("content " + fileNameFace, w.contentOf(fileNameFace));
+        assertEquals("content " + fileNameSwim, w.contentOf(fileNameSwim));
         String status = w.cmd("git status");
         assertTrue("unexpected status " + status, status.contains("working directory clean"));
 
