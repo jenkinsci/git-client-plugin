3e5ad16623e5583444298a5e9adee9c3bb230157
Mark Waite <mark.earl.waite@gmail.com>
2015-06-20 14:32:44 -0600

    [Fix JENKINS-28943] Find windows ssh even if git is absolute path to cmd dir
    The path guesser in getPathToExe correctly checks for cmd and exe
    suffixes, and correctly checks each entry in the PATH environment
    variable.  Prior to this change, it did not check for the absolute
    path having been specified with the cmd directory rather than the bin
    directory.
    
    The user had set C:\Program Files\Git\cmd\git.exe as the git program
    for that slave.  That is an allowed setting, and it executed most git
    commands correctly. It failed on ssh based commands.

--- src/main/java/org/jenkinsci/plugins/gitclient/CliGitAPIImpl.java
+++ src/main/java/org/jenkinsci/plugins/gitclient/CliGitAPIImpl.java
@@ -1463,6 +1463,11 @@ public class CliGitAPIImpl extends LegacyCompatibleGitAPIImpl {
             }
         }
 
+        File userGitFile = new File(userGitExe);
+        if (userGitFile.exists()) {
+            return userGitFile.getAbsolutePath();
+        }
+
         return null;
     }
 
