08db6e0c316029b43eb3644f642c4f4973c39357
Olivier Dagenais <olivier.dagenais@gmail.com>
2016-09-14 10:10:30 -0400

    Add JGitApacheTool and supporting files

---
+++ src/main/java/org/jenkinsci/plugins/gitclient/JGitApacheTool.java
@@ -0,0 +1,46 @@
+package org.jenkinsci.plugins.gitclient;
+
+import hudson.Extension;
+import hudson.plugins.git.GitTool;
+import hudson.tools.ToolProperty;
+import org.jenkinsci.Symbol;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * JGit, configured with the Apache HTTP Client, as {@link hudson.plugins.git.GitTool}
+ */
+public class JGitApacheTool extends GitTool {
+    private static final long serialVersionUID = 1L;
+
+    @DataBoundConstructor
+    public JGitApacheTool(final List<? extends ToolProperty<?>> properties) {
+        super(MAGIC_EXENAME, MAGIC_EXENAME, properties);
+    }
+
+    public JGitApacheTool() {
+        this(Collections.<ToolProperty<?>>emptyList());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public GitTool.DescriptorImpl getDescriptor() {
+        return super.getDescriptor();
+    }
+
+    @Extension @Symbol(MAGIC_EXENAME)
+    public static class DescriptorImpl extends GitTool.DescriptorImpl {
+        @Override
+        public String getDisplayName() {
+            return "JGit with Apache HTTP client";
+        }
+    }
+
+    /**
+     * {@link Git} recognizes this as a magic executable name to use {@link JGitAPIImpl}.
+     */
+    public static final String MAGIC_EXENAME = "jgitapache";
+
+}
