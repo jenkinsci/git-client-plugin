fa5546f9c545cb9b9fe5e928afc7f60c091a1275
Mark Waite <mark.earl.waite@gmail.com>
2017-04-12 21:28:20 -0600

    Include jgitapache in CredentialsTest always
    The command line git portions of the test are only run if command line
    git is new enough.  JGit can be run on any of the supported Java versions,
    so does not need a version check.

--- src/test/java/org/jenkinsci/plugins/gitclient/CredentialsTest.java
+++ src/test/java/org/jenkinsci/plugins/gitclient/CredentialsTest.java
@@ -190,7 +190,7 @@ public class CredentialsTest {
     @Parameterized.Parameters(name = "{2}-{1}-{0}-{5}")
     public static Collection gitRepoUrls() throws MalformedURLException, FileNotFoundException, IOException, InterruptedException, ParseException {
         List<Object[]> repos = new ArrayList<>();
-        String[] implementations = isCredentialsSupported() ? new String[]{"git", "jgit", "jgitapache"} : new String[]{"jgit"};
+        String[] implementations = isCredentialsSupported() ? new String[]{"git", "jgit", "jgitapache"} : new String[]{"jgit", "jgitapache"};
         for (String implementation : implementations) {
             /* Add master repository as authentication test with private
              * key of current user.  Try to test at least one
