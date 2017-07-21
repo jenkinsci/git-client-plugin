a5d2a895cf2ccded99df412a0ea957723c6d10a5
Jacob Keller <jacob.e.keller@intel.com>
2017-07-21 16:51:12 -0700

    [JENKINS-45729] fix getRepository() to work with symref git files
    The current implementation of getRepository() fails to work if the
    repository directory in question has a .git file (known as a symref)
    which points to the real directory. This causes problems if you try to
    directly open a submodule or worktree repository using the git client.
    
    This happens because FileRepositoryBuilder.create() uses .setGitDir() to
    set the git directory which expects only a proper git directory path.
    Instead, set the worktree and have the RepositoryBuilder figure out
    where the git directory is.
    
    This implementation is identical to the JGit implementation, which is
    a nice bonus.
    
    Additionally, add a test which shows the bug. Unfortunately, it does not
    get run on the JGit implementation because of how the test submodules
    are setup.
    
    Without this fix, the git plugin will fail to fetch and checkout
    revisions of git worktrees which use .git symref files, because it uses
    the withRepository in order to obtain the git commit subject.

--- src/test/java/org/jenkinsci/plugins/gitclient/GitAPITestCase.java
+++ src/test/java/org/jenkinsci/plugins/gitclient/GitAPITestCase.java
@@ -2120,6 +2120,35 @@ public abstract class GitAPITestCase extends TestCase {
         assertSubmoduleContents(w.repo);
     }
 
+    /* Opening a git repository in a directory with a symbolic git file instead
+     * of a git directory should function properly. JGit does not implement
+     * renamed submodules, so we the submodules branch won't work with it...
+     */
+    @NotImplementedInJGit
+    public void test_with_repository_works_with_submodule() throws Exception {
+        w = clone(localMirror());
+        assertSubmoduleDirs(w.repo, false, false);
+
+        /* Checkout a branch which includes submodules (in modules directory) */
+        String subBranch = "tests/getSubmodules";
+        String subRefName = "origin/" + subBranch;
+        w.git.checkout().ref(subRefName).branch(subBranch).execute();
+        w.git.submoduleUpdate().recursive(true).execute();
+
+        /* Get a client directly on the ntp submodule */
+        GitClient ntpClient = setupGitAPI(new File(w.repo, "modules/ntp"));
+
+        /* Assert that when we invoke the repository callback it gets a
+         * functioning repository object
+         */
+        ntpClient.withRepository(new RepositoryCallback<Void>() {
+            public Void invoke(final Repository repo, VirtualChannel channel) throws IOException, InterruptedException {
+                assertTrue(repo.getDirectory() + " is not a valid repository",
+                           repo.getObjectDatabase().exists());
+                return null;
+            }});
+    }
+
     private String listDir(File dir) {
         if (dir == null || !dir.exists()) {
             return "";
