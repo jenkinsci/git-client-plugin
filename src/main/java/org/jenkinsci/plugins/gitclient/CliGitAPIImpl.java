ae58bbc5fcbd57cc4320d01cf43a49901e83c5e0
なつき <i@ntk.me>
2018-01-11 11:03:55 -0800

    Use `git clean -ffdx` for git>=2.3.3
    Git will refuse to delete directories with .git sub directory or file unless a second -f is given. This affects also git submodules where the storage area of the removed submodule under .git/modules/ is not removed until -f is given twice.

--- src/main/java/org/jenkinsci/plugins/gitclient/CliGitAPIImpl.java
+++ src/main/java/org/jenkinsci/plugins/gitclient/CliGitAPIImpl.java
@@ -779,7 +779,7 @@ public class CliGitAPIImpl extends LegacyCompatibleGitAPIImpl {
      */
     public void clean() throws GitException, InterruptedException {
         reset(true);
-        launchCommand("clean", "-fdx");
+        launchCommand("clean", "-ffdx");
     }
 
     /** {@inheritDoc} */
@@ -1190,7 +1190,7 @@ public class CliGitAPIImpl extends LegacyCompatibleGitAPIImpl {
     	if (recursive) {
             args.add("--recursive");
     	}
-    	args.add("git clean -fdx");
+    	args.add("git clean -ffdx");
 
     	launchCommand(args);
     }
