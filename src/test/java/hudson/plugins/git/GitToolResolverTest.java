29e97c4a3e0b8f6491154dbae8237a9ec9025918
Mark Waite <mark.earl.waite@gmail.com>
2018-07-31 14:02:53 -0600

    Add BatchCommandInstaller to shouldResolveToolsOnMaster test

--- src/test/java/hudson/plugins/git/GitToolResolverTest.java
+++ src/test/java/hudson/plugins/git/GitToolResolverTest.java
@@ -2,9 +2,11 @@ package hudson.plugins.git;
 
 import hudson.EnvVars;
 import hudson.model.TaskListener;
+import hudson.tools.AbstractCommandInstaller;
+import hudson.tools.BatchCommandInstaller;
 import hudson.tools.CommandInstaller;
 import hudson.tools.InstallSourceProperty;
-import hudson.util.StreamTaskListener;
+import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
 import static org.junit.Assert.*;
@@ -29,15 +31,28 @@ public class GitToolResolverTest {
 
     @Test
     public void shouldResolveToolsOnMaster() throws Exception {
-        TaskListener log = StreamTaskListener.fromStdout();
+        final String label = "master";
+        final String command = "echo Hello";
+        final String toolHome = "TOOL_HOME";
+        AbstractCommandInstaller installer;
+        String expectedSubstring;
+        if (isWindows()) {
+            installer = new BatchCommandInstaller(label, command, toolHome);
+            expectedSubstring = System.getProperty("java.io.tmpdir", "C:\\Temp");
+        } else {
+            installer = new CommandInstaller(label, command, toolHome);
+            expectedSubstring = toolHome;
+        }
         GitTool t = new GitTool("myGit", null, Collections.singletonList(
-                new InstallSourceProperty(Collections.singletonList(
-                        new CommandInstaller("master", "echo Hello", "TOOL_HOME")
-                ))));
+                new InstallSourceProperty(Collections.singletonList(installer))));
         t.getDescriptor().setInstallations(t);
 
         GitTool defaultTool = GitTool.getDefaultInstallation();
         GitTool resolved = (GitTool) defaultTool.translate(j.jenkins, new EnvVars(), TaskListener.NULL);
-        assertThat(resolved.getGitExe(), org.hamcrest.CoreMatchers.containsString("TOOL_HOME"));
+        assertThat(resolved.getGitExe(), org.hamcrest.CoreMatchers.containsString(expectedSubstring));
+    }
+
+    private boolean isWindows() {
+        return File.pathSeparatorChar == ';';
     }
 }
