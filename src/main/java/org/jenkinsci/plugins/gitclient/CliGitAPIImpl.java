aceb48a4b36d645ddd1ff0a66662eeeb53ddc58d
Geoffroy Jabouley <gjabouley@invensense.com>
2019-06-14 14:56:16 +0200

    Use credentials only when a remote is defined
    Fix existing tests

--- src/main/java/org/jenkinsci/plugins/gitclient/CliGitAPIImpl.java
+++ src/main/java/org/jenkinsci/plugins/gitclient/CliGitAPIImpl.java
@@ -886,11 +886,25 @@ public class CliGitAPIImpl extends LegacyCompatibleGitAPIImpl {
                 args.add(fastForwardMode);
                 args.add(rev.name());
 
-                String repoUrl = getRemoteUrl(getDefaultRemote());
-                StandardCredentials cred = credentials.get(repoUrl);
-                if (cred == null) cred = defaultCredentials;
+                /* See JENKINS-45228 */
+                /* Git merge requires authentication in LFS merges, plugin does not authenticate the git merge command */
+                String defaultRemote = null;
+                try {
+                    defaultRemote = getDefaultRemote();
+                } catch (GitException e) {
+                    /* Nothing to do, just keeping defaultRemote = null */
+                }
 
-                launchCommandWithCredentials(args, workspace, cred, repoUrl);
+                if (defaultRemote != null && !defaultRemote.isEmpty()) {
+                    String repoUrl = getRemoteUrl(defaultRemote);
+                    StandardCredentials cred = credentials.get(repoUrl);
+                    if (cred == null) cred = defaultCredentials;
+                    launchCommandWithCredentials(args, workspace, cred, repoUrl);
+                } else {
+                    /* Merge is allowed even if a remote URL is not defined. */
+                    /* If there is no remote URL, there is no need to use credentials in the merge. */
+                    launchCommand(args);
+                }
             }
         };
     }
