7df9737267dbb62df6ce6dd51dd252ec51605103
Rishabh Budhouliya <rishabhbudhouliya@gmail.com>
2020-06-23 13:03:31 +0530

    Add a lighter list of remote repo urls for the benchmark

--- src/test/java/jmh/benchmark/GitClientRedundantFetchBenchmark.java
+++ src/test/java/jmh/benchmark/GitClientRedundantFetchBenchmark.java
@@ -39,8 +39,11 @@ public class GitClientRedundantFetchBenchmark {
         File remoteRepoDir;
         URIish urIish;
 
-        @Param({"https://github.com/ruby/ruby.git",
-                "https://github.com/microsoft/vscode.git"})
+        @Param({"https://github.com/jenkinsci/jenkins-charm.git",
+                "https://github.com/jenkinsci/parameterized-trigger-plugin.git",
+                "https://github.com/jenkinsci/ec2-plugin.git",
+                "https://github.com/jenkinsci/git-plugin.git",
+                "https://github.com/jenkinsci/jenkins.git"})
         String repoUrl;
 
         private File cloneUpstreamRepositoryLocally(File parentDir, String repoUrl) throws Exception {
