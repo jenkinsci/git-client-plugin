6a715721aa59c145f0bdd84f8e39b83807fb0e30
Mark Waite <mark.earl.waite@gmail.com>
2020-07-28 16:23:35 -0600

    Use type safe empty set

--- src/main/java/org/jenkinsci/plugins/gitclient/CliGitAPIImpl.java
+++ src/main/java/org/jenkinsci/plugins/gitclient/CliGitAPIImpl.java
@@ -3570,7 +3570,7 @@ public class CliGitAPIImpl extends LegacyCompatibleGitAPIImpl {
          */
         String[] output = result.split("[\\n\\r]+");
         if (output.length == 0 || (output.length == 1 && output[0].isEmpty())) {
-            return Collections.EMPTY_SET;
+            return Collections.<GitObject>emptySet();
         }
         Pattern pattern = Pattern.compile("(\\p{XDigit}{40})\\s+refs/tags/([^^]+)(\\^\\{\\})?");
         Map<String, ObjectId> tagMap = new HashMap<>();
