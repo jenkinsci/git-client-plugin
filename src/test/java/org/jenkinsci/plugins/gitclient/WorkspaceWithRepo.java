3ff19df7fb0d2c4ff468a30c829782b6880eb993
Jim Klimov <jim@jimklimov.com>
2020-12-10 04:06:43 +0100

    WorkspaceWithRepo.java: localMirror(): normalize() the returned pathname string

--- src/test/java/org/jenkinsci/plugins/gitclient/WorkspaceWithRepo.java
+++ src/test/java/org/jenkinsci/plugins/gitclient/WorkspaceWithRepo.java
@@ -16,6 +16,7 @@ import java.nio.file.AtomicMoveNotSupportedException;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -124,7 +125,8 @@ public class WorkspaceWithRepo {
                         Util.deleteRecursive(tempClonePath.toFile());
                     }
                 }
-                return clone.getPath();
+                // Strip away the "/./" in "...git-client-plugin/./target/..."
+                return Paths.get(clone.getPath()).normalize().toString();
             }
         }
         throw new IllegalStateException();
