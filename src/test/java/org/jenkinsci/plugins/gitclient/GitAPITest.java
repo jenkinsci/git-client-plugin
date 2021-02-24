0ac340c1b90294419f74d862a43116b0b2f8b7de
Ayan Goel <goelaya1998@gmail.com>
2021-02-24 16:08:50 +0530

    Migrated Test Notes Append First Note

--- src/test/java/org/jenkinsci/plugins/gitclient/GitAPITest.java
+++ src/test/java/org/jenkinsci/plugins/gitclient/GitAPITest.java
@@ -796,7 +796,21 @@ public class GitAPITest {
         assertEquals("foo\n\nalpha\rbravo\ncharlie\n\nbar\n\nzot\n", workspace.launchCommand("git", "notes", "show"));
     }
 
+    @Test
+    public void testNotesAppendFirstNote() throws Exception {
+        initializeWorkspace(workspace);
+        workspace.touch(testGitDir, "file1", "");
+        testGitClient.add("file1");
+        workspace.commitEmpty("init");
 
+        testGitClient.appendNote("foo", "commits");
+        assertEquals("foo\n", workspace.launchCommand("git", "notes", "show"));
+        testGitClient.appendNote("alpha\rbravo\r\ncharlie\r\n\r\nbar\n\n\nzot\n\n", "commits");
+        // cgit normalizes CR+LF aggressively
+        // it appears to be collpasing CR+LF to LF, then truncating duplicate LFs down to 2
+        // note that CR itself is left as is
+        assertEquals("foo\n\nalpha\rbravo\ncharlie\n\nbar\n\nzot\n", workspace.launchCommand("git", "notes", "show"));
+    }
 
     private void initializeWorkspace(WorkspaceWithRepo initWorkspace) throws Exception {
         final GitClient initGitClient = initWorkspace.getGitClient();
