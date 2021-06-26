2143625fccbb7c06444600e37517f349f1cdadb3
Mark Waite <mark.earl.waite@gmail.com>
2021-06-26 16:17:24 -0600

    Remove another test

--- src/test/java/org/jenkinsci/plugins/gitclient/LegacyCompatibleGitAPIImplJGitTest.java
+++
@@ -1,9 +0,0 @@
-package org.jenkinsci.plugins.gitclient;
-
-public class LegacyCompatibleGitAPIImplJGitTest extends LegacyCompatibleGitAPIImplTest {
-
-    public LegacyCompatibleGitAPIImplJGitTest() {
-        gitImpl = "jgit";
-    }
-
-}
