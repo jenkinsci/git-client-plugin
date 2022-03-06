bb119162d348efd27be301ef297282f2aff4a3dd
hrushi20 <hrushikesh20thegreat@gmail.com>
2022-03-07 04:33:38 +0530

    moved testGetSubmodules

--- src/test/java/org/jenkinsci/plugins/gitclient/GitAPITestUpdate.java
+++ src/test/java/org/jenkinsci/plugins/gitclient/GitAPITestUpdate.java
@@ -483,6 +483,32 @@ public abstract class GitAPITestUpdate {
         }
     }
 
+    /* Shows the JGit submodule update is broken now that tests/getSubmodule includes a renamed submodule */
+    @Test
+    public void testGetSubmodules() throws Exception {
+        w.init();
+        w.git.clone_().url(localMirror()).repositoryName("sub_origin").execute();
+        w.git.checkout().ref("sub_origin/tests/getSubmodules").branch("tests/getSubmodules").execute();
+        List<IndexEntry> r = w.git.getSubmodules("HEAD");
+        assertEquals(
+                "[IndexEntry[mode=160000,type=commit,file=modules/firewall,object=978c8b223b33e203a5c766ecf79704a5ea9b35c8], " +
+                        "IndexEntry[mode=160000,type=commit,file=modules/ntp,object=b62fabbc2bb37908c44ded233e0f4bf479e45609], " +
+                        "IndexEntry[mode=160000,type=commit,file=modules/sshkeys,object=689c45ed57f0829735f9a2b16760c14236fe21d9]]",
+                r.toString()
+        );
+        w.git.submoduleInit();
+        w.git.submoduleUpdate().execute();
+
+        assertTrue("modules/firewall does not exist", w.exists("modules/firewall"));
+        assertTrue("modules/ntp does not exist", w.exists("modules/ntp"));
+        // JGit submodule implementation doesn't handle renamed submodules
+        if (w.igit() instanceof CliGitAPIImpl) {
+            assertTrue("modules/sshkeys does not exist", w.exists("modules/sshkeys"));
+        }
+        assertFixSubmoduleUrlsThrows();
+    }
+
+
     /* Shows the submodule update is broken now that tests/getSubmodule includes a renamed submodule */
     @NotImplementedInJGit
     @Test
