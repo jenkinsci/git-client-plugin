b4c90d5a9584796242ddfb007ab0098d697a8812
hrushi20 <hrushikesh20thegreat@gmail.com>
2022-03-07 04:29:03 +0530

    moved testSubmoduleUpdate

--- src/test/java/org/jenkinsci/plugins/gitclient/GitAPITestCase.java
+++ src/test/java/org/jenkinsci/plugins/gitclient/GitAPITestCase.java
@@ -1000,27 +1000,6 @@ public abstract class GitAPITestCase extends TestCase {
         assertFixSubmoduleUrlsThrows();
     }
 
-    /* Shows the submodule update is broken now that tests/getSubmodule includes a renamed submodule */
-    @NotImplementedInJGit
-    public void test_submodule_update() throws Exception {
-        w.init();
-        w.git.clone_().url(localMirror()).repositoryName("sub2_origin").execute();
-        w.git.checkout().branch("tests/getSubmodules").ref("sub2_origin/tests/getSubmodules").deleteBranchIfExist(true).execute();
-        w.git.submoduleInit();
-        w.git.submoduleUpdate().execute();
-
-        assertTrue("modules/firewall does not exist", w.exists("modules/firewall"));
-        assertTrue("modules/ntp does not exist", w.exists("modules/ntp"));
-        // JGit submodule implementation doesn't handle renamed submodules
-        if (w.igit() instanceof CliGitAPIImpl) {
-            assertTrue("modules/sshkeys does not exist", w.exists("modules/sshkeys"));
-        }
-        assertFixSubmoduleUrlsThrows();
-
-        String shallow = Paths.get(".git", "modules", "module", "1", "shallow").toString();
-        assertFalse("shallow file existence: " + shallow, w.exists(shallow));
-    }
-
     /*
      * core.symlinks is set to false by git for WIndows.
      * It is not set on Linux.
