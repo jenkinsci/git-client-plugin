f3ae2a63557a0a72002c3a699a17e94b8ab007a5
Mark Waite <mark.earl.waite@gmail.com>
2022-10-23 22:40:04 -0600

    Remove unused imports

--- src/main/java/org/jenkinsci/plugins/gitclient/verifier/ManuallyProvidedKeyVerifier.java
+++ src/main/java/org/jenkinsci/plugins/gitclient/verifier/ManuallyProvidedKeyVerifier.java
@@ -8,7 +8,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
--- src/test/java/org/jenkinsci/plugins/gitclient/GitAPITest.java
+++ src/test/java/org/jenkinsci/plugins/gitclient/GitAPITest.java
@@ -40,7 +40,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 import java.io.File;
 import java.io.StringWriter;
 import java.nio.file.Files;
