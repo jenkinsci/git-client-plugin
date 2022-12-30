b85f61352131973528ce954a57d8b5505a6931da
Mark Waite <mark.earl.waite@gmail.com>
2022-12-29 21:36:14 -0700

    Remove unused import

--- src/test/java/org/jenkinsci/plugins/gitclient/GitClientTest.java
+++ src/test/java/org/jenkinsci/plugins/gitclient/GitClientTest.java
@@ -18,7 +18,6 @@ import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
