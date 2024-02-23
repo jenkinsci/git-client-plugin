17ee0128745e00858fbb7dd87c3297fa9fe1bc6a
AniketNS <aniketshitole565@gmail.com>
2024-02-23 14:29:14 +0530

    made some changes

--- src/main/java/hudson/plugins/git/Revision.java
+++ src/main/java/hudson/plugins/git/Revision.java
@@ -121,9 +121,10 @@ public class Revision implements java.io.Serializable, Cloneable {
 
     @Override
     public Revision clone() {
-        Revision clone;
         try {
-            clone = (Revision) super.clone();
+            Revision clone = (Revision) super.clone();
+            clone.branches = new ArrayList<>(branches);
+            return clone;
         } catch (CloneNotSupportedException e) {
             throw new RuntimeException("Error cloning Revision", e);
         }
