e4550dfb16f2e8ecd144222c39689dbdad2135b3
Olivier Lamy <olamy@apache.org>
2024-06-06 20:17:39 +1000

    restore NoHostKeyVerifierTest
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/test/java/org/jenkinsci/plugins/gitclient/verifier/NoHostKeyVerifierTest.java
+++ src/test/java/org/jenkinsci/plugins/gitclient/verifier/NoHostKeyVerifierTest.java
@@ -4,9 +4,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.jenkinsci.plugins.gitclient.verifier.KnownHostsTestUtil.isKubernetesCI;
 
+import hudson.model.StreamBuildListener;
 import hudson.model.TaskListener;
+
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.time.Duration;
+
+import org.awaitility.Awaitility;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -20,15 +25,28 @@ public class NoHostKeyVerifierTest {
     }
 
     @Test
-    public void testVerifyServerHostKey() throws IOException {
+    public void verifyServerHostKey() throws IOException {
         if (isKubernetesCI()) {
             return; // Test fails with connection timeout on ci.jenkins.io kubernetes agents
         }
-        //        JGitConnection jGitConnection = new JGitConnection("github.com", 22);
-        // Should not fail because verifyServerHostKey always true
-        // FIXME ol
-        //        jGitConnection.connect(verifier.forJGit(TaskListener.NULL));
+
+        NoHostKeyVerifier acceptFirstConnectionVerifier = new NoHostKeyVerifier();
+
+        KnownHostsTestUtil.connectToHost(
+                        "github.com",
+                        22,
+                        null,
+                        acceptFirstConnectionVerifier.forJGit(StreamBuildListener.fromStdout()),
+                        s -> {
+                            assertThat(s.isOpen(), is(true));
+                            Awaitility.await().atMost(Duration.ofSeconds(45)).until(() -> s.getServerKey() != null);
+                            assertThat(KnownHostsTestUtil.checkKeys(s), is(true));
+                            // Should not fail because verifyServerHostKey always true
+                            return true;
+                        })
+                .close();
     }
+
 
     @Test
     public void testVerifyHostKeyOption() throws IOException {
