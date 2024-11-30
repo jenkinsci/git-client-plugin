b38cc2de03450a6d2c0bf3f6d8a8b42913262574
Mark Waite <mark.earl.waite@gmail.com>
2024-11-30 07:13:29 -0700

    Test with JGit 7.1 packRefs() command instead of internal GC method
    The internal GC method has been removed in JGit 7.1 with the additon
    of PackRefsCommand.
    
    Use try with resources on the JGit Git object because it implements
    AutoClosable.

--- src/test/java/org/jenkinsci/plugins/gitclient/JGitLightweightTagTest.java
+++ src/test/java/org/jenkinsci/plugins/gitclient/JGitLightweightTagTest.java
@@ -14,7 +14,6 @@ import hudson.EnvVars;
 import hudson.model.TaskListener;
 import hudson.plugins.git.GitObject;
 import java.io.File;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
@@ -81,21 +80,17 @@ public class JGitLightweightTagTest {
         }
     }
 
-    private void packRefs() throws IOException {
-        try (FileRepository repo = new FileRepository(repoRootGitDir)) {
-            org.eclipse.jgit.internal.storage.file.GC gc;
-            gc = new org.eclipse.jgit.internal.storage.file.GC(repo);
-            gc.packRefs();
+    private void packRefs() throws Exception {
+        try (org.eclipse.jgit.api.Git jgit = new org.eclipse.jgit.api.Git(new FileRepository(repoRootGitDir))) {
+            jgit.packRefs().setAll(true).call();
         }
     }
 
     // No flavor of GitClient has a tag(String) API, only tag(String,String).
     // But sometimes we want a lightweight a.k.a. non-annotated tag.
     private void lightweightTag(String tagName) throws Exception {
-        try (FileRepository repo = new FileRepository(repoRootGitDir)) {
-            // Collides with implicit org.jenkinsci.plugins.gitclient.Git.
-            org.eclipse.jgit.api.Git jgitAPI = org.eclipse.jgit.api.Git.wrap(repo);
-            jgitAPI.tag().setName(tagName).setAnnotated(false).call();
+        try (org.eclipse.jgit.api.Git jgit = new org.eclipse.jgit.api.Git(new FileRepository(repoRootGitDir))) {
+            jgit.tag().setName(tagName).setAnnotated(false).call();
         }
     }
 
