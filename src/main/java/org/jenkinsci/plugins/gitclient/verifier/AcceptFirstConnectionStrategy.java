4f72c445b959551e69ef268e64065ac925a2d9a9
Mark Waite <mark.earl.waite@gmail.com>
2025-01-04 08:43:36 -0700

    [JENKINS-73923] Add javadoc for SSH host key verification strategies (#1237)
    https://issues.jenkins.io/browse/JENKINS-73923 notes that
    https://jenkins.io/doc/developer/extensions/git-client/ has
    no documentation.  It says "This extension point has no Javadoc
    documentation".
    
    Provide the documentation for the extension point and the four
    implementations of the extension point so that extension point
    documentation will include documentation after the next release of the
    git client plugin.
    
    Much of the text is copied from the README or derived from the text in
    the README.
    
    Testing done
    
    Checked that the Javadoc renders as expected in Chrome.

--- src/main/java/org/jenkinsci/plugins/gitclient/verifier/AcceptFirstConnectionStrategy.java
+++ src/main/java/org/jenkinsci/plugins/gitclient/verifier/AcceptFirstConnectionStrategy.java
@@ -5,13 +5,24 @@ import hudson.Extension;
 import hudson.model.Descriptor;
 import org.kohsuke.stapler.DataBoundConstructor;
 
+/**
+ * Accept known hosts strategy for the {@link SshHostKeyVerificationStrategy host key verification strategy extension point}.
+ *
+ * <p>Remembers the first host key encountered for each git server and requires that the same host key must be used for later access.
+ * This is usually the most convenient setting for administrators while still providing ssh host key verification
+ */
 public class AcceptFirstConnectionStrategy extends SshHostKeyVerificationStrategy<AcceptFirstConnectionVerifier> {
 
+    /**
+     * Creates a secure shell host key verification strategy that accepts known hosts on first connection.
+     * Remembers the first host key encountered for each git server and requires that the same host key must be used for later access.
+     */
     @DataBoundConstructor
     public AcceptFirstConnectionStrategy() {
         // stapler needs @DataBoundConstructor
     }
 
+    /** {@inheritDoc} */
     @Override
     public AcceptFirstConnectionVerifier getVerifier() {
         return new AcceptFirstConnectionVerifier();
