865694dabcd715b983c7d6b4f33d04a136c02726
Mark Waite <mark.earl.waite@gmail.com>
2025-03-13 20:07:42 -0600

    Note that PushTest cleanup works locally

--- src/test/java/org/jenkinsci/plugins/gitclient/PushTest.java
+++ src/test/java/org/jenkinsci/plugins/gitclient/PushTest.java
@@ -244,7 +244,8 @@ public class PushTest {
                 Thread.sleep(4021L);
                 FileUtils.deleteDirectory(bareRepo);
             } catch (IOException ioe) {
-                // TODO Understand why this fails with JGit 7.2.0 and not earlier
+                // TODO Understand why this fails with JGit 7.2.0 and not earlier on ci.jenkins.io
+                // Does not fail on my local Windows agents
                 System.out.println("PushTest ignored Windows IOException removing bare repo " + bareRepo);
             }
         } else {
